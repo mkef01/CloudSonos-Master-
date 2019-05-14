1f80352daa26445b9a4015168c8bcaee068844bd
Elmer FIgueroa <elmerfig@live.com>
2019-05-13 23:13:17 -0600

    Agregado LINQ al proyecto listo para servir APIS

---
+++ CloudSonos/Controllers/AdministrativoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CloudSonos.Models;
+
+namespace CloudSonos.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        private readonly NubeDataContext _context;
+
+        
+        // Agregar Genero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index (generos datos)
+        {
+            using (NubeDataContext nubeDataContext = new NubeDataContext())
+            {
+                try
+                {
+                    nubeDataContext.generos.InsertOnSubmit(datos);
+                    nubeDataContext.SubmitChanges();
+                }catch (Exception e){
+                    Console.WriteLine(e);
+                    throw;
+
+                }
+            }
+            return View();
+        }
+    }
+}
